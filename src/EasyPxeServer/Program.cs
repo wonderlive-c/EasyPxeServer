44fc8725977e87e9acdbb3ccbbdb234179992b72
cuiwulin <cuiwulin@126.com>
2025-09-28 14:23:02 +0800

    windows运行环境限制条件及其他微调

--- src/EasyPxeServer/Program.cs
+++ src/EasyPxeServer/Program.cs
@@ -2,12 +2,43 @@
 using Microsoft.AspNetCore.Mvc;
 using EasyPxeServer.Services;
 using Serilog;
+using System.Security.Principal;
+using System.Reflection;
+using System.IO;
 
-Log.Logger = new LoggerConfiguration().WriteTo.File("log.txt").WriteTo.Console().CreateBootstrapLogger();
+
+if (Environment.OSVersion.Platform is PlatformID.Win32Windows or PlatformID.Win32NT or PlatformID.Win32S or PlatformID.WinCE)
+{
+    // 检查是否具有管理员权限
+    var isAdmin = new WindowsPrincipal(WindowsIdentity.GetCurrent()).IsInRole(WindowsBuiltInRole.Administrator);
+    if (!isAdmin)
+    {
+        Console.WriteLine("错误: 程序需要管理员权限才能运行。");
+        Console.WriteLine("请右键点击程序并选择'以管理员身份运行'。");
+        Console.WriteLine("按任意键退出...");
+        Console.ReadKey();
+        return;
+    }
+
+    // 检查是否在C盘运行
+    var currentDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+    if (currentDirectory != null
+     && currentDirectory.StartsWith("C:\\", StringComparison.OrdinalIgnoreCase))
+    {
+        Console.WriteLine("错误: 程序禁止在C盘运行。");
+        Console.WriteLine("请将程序移至其他磁盘后再运行。");
+        Console.WriteLine("按任意键退出...");
+        Console.ReadKey();
+        return;
+    }
+}
+
+
+Log.Logger = new LoggerConfiguration().WriteTo.File("log.txt").WriteTo.Console().MinimumLevel.Debug().CreateBootstrapLogger();
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Configuration.AddJsonFile(@"appsettings.log.json");
-builder.Host.UseSerilog();
+builder.Logging.AddSerilog();
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
@@ -39,6 +70,8 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.Environment.EnvironmentName = "Development";
 
 app.UseHttpsRedirection();
 
